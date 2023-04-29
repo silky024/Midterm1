d5ba35faba1f067944940b138a01ebf7a84df4a3
silky024 <89177463+silky024@users.noreply.github.com>
2023-04-29 13:52:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const nreq = require('./lib/NReq')
+//console.log(nreq.request('POST','http','localhost',10001,'/player/get/15001'))
+//console.log(nreq.request('GET','http','localhost',10001,'/player/get/15001'))
+/*
+nreq.post('http', 'localhost:9818/hi')
+nreq.request('GET', 'https','covid19.th-stat.com/api/open/today')
+let payload = {"player_id" : 15003}
+nreq.post('http','localhost:10002/reward/collected', payload)
+
+let payload = {"player_id" : 15001}
+nreq.post('http','localhost:10002/reward/collected', payload) */
+
 async function showRequest()
 {
-    console.log( await nreq.request('POST','http','localhost',10001,'/player/get/15001'))
-    console.log( await nreq.request('GET','http','localhost',10001,'/player/get/15005'))
-    console.log( await nreq.request('POST','http','localhost',10001,'/player/get/15005'))
+    //console.log( await nreq.request('POST','http','localhost',10001,'/player/get/15001'))
+    //console.log( await nreq.request('GET','http','localhost',10001,'/player/get/15005'))
+   //console.log( await nreq.request('POST','http','localhost',10001,'/player/get/15005'))
+
+   console.log( await nreq.request('POST','http','localhost',10002,'/reward/collect/15001'))
+
+
     
 
     
